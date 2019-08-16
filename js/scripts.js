1ef7270d2aec87cb0591392877ead58dda564810
Carrie Schmid <carriepederson7@gmail.com>
2019-08-16 11:46:37 -0700

    Add initial branching logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
-  $("$#quiz").submit(function(event){
+  $("#quiz").submit(function(event){
     event.preventDefault();
     $(".results").hide();
+
+
 
     var type=$("input:radio[name=type]:checked").val();
     var setting=$("input:radio[name=setting]:checked").val();
@@ -10,9 +12,17 @@ $(document).ready(function(){
     var graphics=$("input:radio[name=graphics]:checked").val();
 
     if (!type || !setting || !frontBack || !earning || !graphics){
-      alert("Please answer questions.")
-    }else if ()
+      alert("Please answer all of the questions.")
+    }else if (earning === "very-important"){
+      $("#javascript").show();
+    }else if(type === "web"){
+      $("#php").show();
+    }else if(type === "mobile"){
+      $("#c#").show();
+    }else {
+      alert("Please answer questions.");
+    }
 
-  )};
+  });
 
-)};
+});
