92cdb0aeb0858f20814fd95404d9a886878723bc
Carrie Schmid <carriepederson7@gmail.com>
2019-08-16 14:23:26 -0700

    Add branching logic updates.

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,15 @@ $(document).ready(function(){
 
     var type=$("input:radio[name=type]:checked").val();
     var setting=$("input:radio[name=setting]:checked").val();
-    var frontBack=$("input:radio[name=front-back]:checked").val();
+    var frontBack=$("input:radio[name=frontBack]:checked").val();
     var earning=$("input:radio[name=earning]:checked").val();
     var graphics=$("input:radio[name=graphics]:checked").val();
 
     if (!type || !setting || !frontBack || !earning || !graphics){
       alert("Please answer all of the questions.")
-    }else if(frontBack === "back-end" && (graphics === "high" || "medium" || type === "mobile")|| "game"){
+    }else if((type === "mobile"|| "game") && (setting === "traditional" || "start-up") && (graphics === "high" || "medium" ) && frontBack=== "back-end"){
       $("#csharp").show();
-    }else if(frontBack === "back-end" && (setting === "freelance" || type === "web" || earning === "important" || "somewhat-important")){
+    }else if(type === "web" && setting === "freelance" && (earning === "important" || "somewhat-important") && graphics === "low" && frontBack === "back-end"){
       $("#php").show();
     }else {
       $("#javascript").show();
